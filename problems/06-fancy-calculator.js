522d536f160f6c284470c748c66ccfc0911b4d23
Alexander <allanosinhb@gmail.com>
2023-10-09 21:20:16 -0700

    finished

--- problems/06-fancy-calculator.js
+++ problems/06-fancy-calculator.js
@@ -1,4 +1,17 @@
-// Your code here
+const Calculator = require('./02-calculator')
+
+const FancyCalculator = class FancyCalculator extends Calculator {
+	setTotal(newTotal) {
+		return this.total = newTotal
+	}
+	modulo(num) {
+		return this.total = this.total % num
+	}
+	squared() {
+		return this.total *= this.total
+	}
+}
+
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
